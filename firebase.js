b0f907bd66dec74d3963526ff766fa713bc33576
Kyle Banta <kbanta11@gmail.com>
2024-05-04 20:56:10 -0700

    comment out service account

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require('./firebase-admin-key.json');
+//const serviceAccount = require('./firebase-admin-key.json');
 const base64Credential = process.env.FIREBASE_CREDENTIALS_BASE64;
 const decodedCredential = JSON.parse(Buffer.from(base64Credential ?? '', 'base64').toString('utf-8'));
 
