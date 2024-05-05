64f640f390caac23d78ecb570128ed413d404c63
Kyle Banta <kbanta11@gmail.com>
2024-05-05 03:03:56 -0700

    added endpoint body

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ app.get('/start/:fid', async (req, res) => {
                     'Content-Type': 'application/json'
                 }
             });
-            console.log(`openRankResult: ${openRankResponse?.result}`)
-            const openRank = openRankResponse?.result?.[0]?.response
+            console.log(`openRankResult: ${openRankResponse?.data?.result}`)
+            const openRank = openRankResponse?.data?.result?.[0]?.response
             const engagementScores = await axios.post(openrankURL, [userFid], {
                 headers: {
                     'Content-Type': 'application/json'
