21d607240087136e9730bf34f0ee31db103cf32b
Kyle Banta <kbanta11@gmail.com>
2024-05-04 20:19:44 -0700

    use base64 firebase credential

--- firebase.js
+++ firebase.js
@@ -5,6 +5,6 @@ const base64Credential = process.env.FIREBASE_CREDENTIALS_BASE64;
 const decodedCredential = JSON.parse(Buffer.from(base64Credential ?? '', 'base64').toString('utf-8'));
 
 if (admin.apps.length === 0) {
-    admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
+    admin.initializeApp({credential: admin.credential.cert(decodedCredential)});
 }
 export const db = admin.firestore();
